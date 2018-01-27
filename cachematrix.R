001b9391569dad6857455652f8b5430ea6aed339
GrannyAnnie <35352691+GrannyAnnie@users.noreply.github.com>
2018-01-26 16:10:06 -0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special "vector", which is really a list containing 
+## a function to set and get the value of the vector, and set and get the value of the mean. 
 
-## Write a short comment describing this function
+## This matrix will be used as the input to the subsequent cacheSolve matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## uses <<- operator to assign a value to an object in an environment 
+  ## that is different from the current environment. 
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve matrix uses the matrix created in makeCacheMatrix as its input
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if (!is.null(inv)){
+    message ("getting cached data")
+    return (inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setinv(inv)
+  return(inv)
 }
